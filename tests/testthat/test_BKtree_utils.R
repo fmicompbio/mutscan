93848b315ca5a16c3e486fbe552b024af5ac31e9
mbstadler <stadler.michael@gmail.com>
2020-08-04 17:55:26 +0200

    comment out tests for collapseSeqs

--- tests/testthat/test_BKtree_utils.R
+++ tests/testthat/test_BKtree_utils.R
@@ -44,34 +44,34 @@ test_that("low-level BK tree functions work as expected", {
 })
 
 
-test_that("collapseSeqs works as expected", {
-  # create random k-mers
-  set.seed(41)
-  k <- 6L
-  n <- 1000L
-  alph <- c("A","C","G","T")
-  seqs <- unlist(lapply(seq_len(n),
-                        function(i) paste(sample(alph, size = k, replace = TRUE),
-                                          collapse = "")))
-
-  # pre-fligh checks
-  expect_error(collapseSeqs(seqs = 1))
-  expect_error(collapseSeqs(seqs = character(0)))
-  expect_error(collapseSeqs(seqs = c("A","AA")))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), counts = c("AA","AC")))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), counts = 1:3))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), method = "non_existing"))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), tol = -1))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), tol = 1:2))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), verbose = "no"))
-  expect_error(collapseSeqs(seqs = c("AA","AC"), verbose = c(TRUE,FALSE)))
-  
-  # add sequences
-  res1 <- collapseSeqs(seqs, tol = 1)
-  res2 <- collapseSeqs(rev(seqs), seq_along(seqs), tol = 1)
-  res3 <- collapseSeqs(seqs, tol = k)
-  expect_is(res1, "list")
-  expect_length(unlist(res1), length(unique(seqs)))
-  expect_identical(res1, res2)
-  expect_length(res3, 1L)
-})
+# test_that("collapseSeqs works as expected", {
+#   # create random k-mers
+#   set.seed(41)
+#   k <- 6L
+#   n <- 1000L
+#   alph <- c("A","C","G","T")
+#   seqs <- unlist(lapply(seq_len(n),
+#                         function(i) paste(sample(alph, size = k, replace = TRUE),
+#                                           collapse = "")))
+# 
+#   # pre-fligh checks
+#   expect_error(collapseSeqs(seqs = 1))
+#   expect_error(collapseSeqs(seqs = character(0)))
+#   expect_error(collapseSeqs(seqs = c("A","AA")))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), counts = c("AA","AC")))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), counts = 1:3))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), method = "non_existing"))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), tol = -1))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), tol = 1:2))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), verbose = "no"))
+#   expect_error(collapseSeqs(seqs = c("AA","AC"), verbose = c(TRUE,FALSE)))
+#   
+#   # add sequences
+#   res1 <- collapseSeqs(seqs, tol = 1)
+#   res2 <- collapseSeqs(rev(seqs), seq_along(seqs), tol = 1)
+#   res3 <- collapseSeqs(seqs, tol = k)
+#   expect_is(res1, "list")
+#   expect_length(unlist(res1), length(unique(seqs)))
+#   expect_identical(res1, res2)
+#   expect_length(res3, 1L)
+# })
