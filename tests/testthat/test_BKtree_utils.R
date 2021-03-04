388748b41a60e0dc6841c845a4cb011f576e50d9
mbstadler <stadler.michael@gmail.com>
2021-03-04 12:41:25 +0100

    add tests for BKtree("hamming") and BKtree("levenshtein")

--- tests/testthat/test_BKtree_utils.R
+++ tests/testthat/test_BKtree_utils.R
@@ -61,76 +61,121 @@ test_that("low-level BKtree wrapper functions work as expected", {
   seqs <- c(seqs, paste0(setdiff(alph, substr(seqs[1], 1, 1)),
                          substr(seqs[1], 2, k)))
   
-  # load module and create tree instance
+  # load module and create tree instances
   bk <- Rcpp::Module("mod_BKtree", PACKAGE = "mutscan")
   BKtree <- bk$BKtree
+  expect_error(new(BKtree, "error"))
   tree <- new(BKtree)
+  tree2 <- new(BKtree, "levenshtein")
   expect_is(bk, "Module")
   expect_is(BKtree, "C++Class")
   expect_is(tree, "Rcpp_BKtree")
+  expect_is(tree2, "Rcpp_BKtree")
+  expect_identical(tree$distance_metric(), "hamming")
+  expect_identical(tree2$distance_metric(), "levenshtein")
   
   # add sequences
   expect_equal(tree$size, 0)
-  for (s in seqs)
+  expect_equal(tree2$size, 0)
+  for (s in seqs) {
     tree$insert(s)
+    tree2$insert(s)
+  }
   expect_equal(tree$size, n)
+  expect_equal(tree2$size, n)
   tree$insert(seqs[1])
+  tree2$insert(seqs[1])
   expect_equal(tree$size, n)
-
+  expect_equal(tree2$size, n)
+  
   # check presence
   expect_true(tree$has(seqs[1], 0))
+  expect_true(tree2$has(seqs[1], 0))
   expect_true(tree$has(seqs[1], k))
+  expect_true(tree2$has(seqs[1], k))
   expect_false(tree$has("non_existing", 0))
+  expect_false(tree2$has("non_existing", 0))
   
   # get first element
   expect_identical(tree$first(), seqs[1])
+  expect_identical(tree2$first(), seqs[1])
   
   # memory size
   m <- tree$capacity()
   expect_is(m, "integer")
   expect_true(m > 0 && m < n * k * 8)
+  expect_identical(tree2$capacity(), m)
   
   # print tree
   expect_output(tree$print(), paste0("^", n, ".+0: ", seqs[1]))
-
+  expect_output(tree2$print(), paste0("^", n, ".+0: ", seqs[1]))
+  
   # search  similar sequences
-  res00 <- tree$search(seqs[1], 0)
-  res01 <- tree$search(seqs[1], 1)
-  res24 <- tree$search(seqs[1], 24)
-  expect_is(res00, "character")
-  # ground truth e.g. from:
-  # sum(stringdist::stringdist(seqs[1], seqs, method = "hamming") <= 24)
-  expect_length(res00, 1L)
-  expect_length(res01, 5L)
-  expect_length(res24, 14L)
-
+  # ... hamming distances
+  # ground truth from: sum(stringdist::stringdist(seqs[1], seqs, method = "hamming") <= 24)
+  res <- tree$search(seqs[1], 0)
+  expect_is(res, "character")
+  expect_length(res, 1L)
+  expect_length(tree$search(seqs[1],  1),  5L)
+  expect_length(tree$search(seqs[1], 24), 14L)
   expect_length(tree$search(seqs[2],  2),  1L)
   expect_length(tree$search(seqs[2], 17),  1L)
   expect_length(tree$search(seqs[2], 23),  9L)
   expect_length(tree$search(seqs[2], 26), 16L)
   expect_length(tree$search(seqs[2], 27), 20L)
   expect_length(tree$search(seqs[2], 30), 20L)
-
+  expect_true(all(tree$search(seqs[2], 23) %in% seqs))
+  # ... levenshtein distances
+  # ground truth from: sum(stringdist::stringdist(seqs[1], seqs, method = "lv") <= 24)
+  res <- tree2$search(seqs[1], 0)
+  expect_is(res, "character")
+  expect_length(res, 1L)
+  expect_length(tree2$search(seqs[1],  1),  5L)
+  expect_length(tree2$search(seqs[1], 18),  8L)
+  expect_length(tree2$search(seqs[2],  2),  1L)
+  expect_length(tree2$search(seqs[2], 17),  3L)
+  expect_length(tree2$search(seqs[2], 18),  6L)
+  expect_length(tree2$search(seqs[2], 19), 13L)
+  expect_length(tree2$search(seqs[2], 20), 18L)
+  expect_length(tree2$search(seqs[2], 30), 20L)
+  expect_true(all(tree2$search(seqs[2], 19) %in% seqs))
+  
   # remove sequences
-  tree$remove("non_existing")
+  expect_identical(tree$remove("non_existing"), NULL)
+  expect_identical(tree2$remove("non_existing"), NULL)
   expect_equal(tree$size, n)
+  expect_equal(tree2$size, n)
   r <- ceiling(n * 0.7)
-  for (i in seq_len(r))
+  for (i in seq_len(r)) {
     tree$remove(seqs[i])
+    tree2$remove(seqs[i])
+  }
   expect_equal(tree$size, n - r)
-  tree$remove_all()
+  expect_equal(tree2$size, n - r)
+  expect_identical(tree$remove_all(), NULL)
+  expect_identical(tree2$remove_all(), NULL)
   expect_equal(tree$size, 0)
-
+  expect_equal(tree2$size, 0)
+  
   # check existing elements
-  tree$remove_all()
-  for (s in seqs)
+  expect_identical(tree$remove_all(), NULL)
+  expect_identical(tree2$remove_all(), NULL)
+  for (s in seqs) {
     tree$insert(s)
-  res <- tree$get_all()
+    tree2$insert(s)
+  }
+  expect_is(res <- tree$get_all(), "character")
+  expect_is(res2 <- tree2$get_all(), "character")
   expect_identical(seqs, res)
+  expect_identical(seqs, res2)
   set.seed(43)
   i <- sample(length(seqs), round(length(seqs) / 3))
-  for (ii in i)
+  for (ii in i) {
     tree$remove(seqs[ii])
-  res <- tree$get_all()
+    tree2$remove(seqs[ii])
+  }
+  expect_is(res <- tree$get_all(), "character")
+  expect_is(res2 <- tree2$get_all(), "character")
   expect_identical(seqs[-i], res)
+  expect_identical(seqs[-i], res2)
 })
