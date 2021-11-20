6c6a3b72eb31fbc996f45ab3b7753159f81eb876
Charlotte Soneson <charlottesoneson@gmail.com>
2021-11-20 11:38:16 +0100

    Use utils in plotTotals

--- R/plotTotals.R
+++ R/plotTotals.R
@@ -24,12 +24,13 @@
 #' 
 plotTotals <- function(se, selAssay = "counts", groupBy = NULL) {
     stopifnot(is(se, "SummarizedExperiment"))
-    stopifnot(length(selAssay) == 1 && is.character(selAssay) && 
-                  selAssay %in% SummarizedExperiment::assayNames(se))
-    stopifnot(is.null(groupBy) ||
-                  (length(groupBy) == 1 && is.character(groupBy) && 
-                       groupBy %in% colnames(SummarizedExperiment::rowData(se))))
-    
+    .assertScalar(selAssay, type = "character",
+                  validValues = SummarizedExperiment::assayNames(se))
+    if (!is.null(groupBy)) {
+        .assertScalar(groupBy, type = "character",
+                      validValues = colnames(SummarizedExperiment::rowData(se)))
+    }
+
     ## Extract the assay matrix and reformat relevant parts of it for plotting
     selAssayMat <- SummarizedExperiment::assay(se, selAssay)
     if (!is.null(groupBy)) {
