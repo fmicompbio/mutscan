cc8d7686a85afaf8df5a637084149e4250fa91fc
Charlotte Soneson <charlottesoneson@gmail.com>
2021-11-29 11:48:08 +0100

    Propagate information about the number of mutated bases/codons in summarizeExperiment

--- tests/testthat/test_summarizeExperiment.R
+++ tests/testthat/test_summarizeExperiment.R
@@ -112,6 +112,15 @@ test_that("summarizeExperiment works as expected with reads output", {
     
     expect_equal(S4Vectors::metadata(se)$parameters[["sample1"]], out1$parameters)
     expect_equal(S4Vectors::metadata(se)$parameters[["sample2"]], out2$parameters)
+
+    ## Check that the number of mutated codons are correct (=equal to the number of 
+    ## entries in the mutant name that don't contain WT)    
+    expect_equal(sapply(strsplit(SummarizedExperiment::rowData(se)$mutantName, "_"), 
+                        function(w) length(w[!grepl("WT", w)])),
+                 SummarizedExperiment::rowData(se)$minNbrMutCodons, ignore_attr = TRUE)
+    expect_equal(sapply(strsplit(SummarizedExperiment::rowData(se)$mutantName, "_"), 
+                        function(w) length(w[!grepl("WT", w)])),
+                 SummarizedExperiment::rowData(se)$maxNbrMutCodons, ignore_attr = TRUE)
 })
 
 test_that("summarizeExperiment works as expected with umis output", {
