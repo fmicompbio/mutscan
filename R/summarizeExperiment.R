063d691d5ae959fcba3649154f760357e2a4dc1a
Charlotte Soneson <charlottesoneson@gmail.com>
2023-02-15 16:52:02 +0100

    Add example to summarizeExperiment

--- R/summarizeExperiment.R
+++ R/summarizeExperiment.R
@@ -50,6 +50,35 @@
 #' @importFrom tidyr unite separate_rows
 #' @importFrom rlang .data
 #' @importFrom stats setNames
+#' 
+#' @examples 
+#' ## Input sample
+#' inp <- digestFastqs(
+#'     fastqForward = system.file("extdata", "cisInput_1.fastq.gz", 
+#'                                package = "mutscan"), 
+#'     elementsForward = "SUCV", elementLengthsForward = c(1, 10, 18, 96), 
+#'     constantForward = "AACCGGAGGAGGGAGCTG", 
+#'     wildTypeForward = c(FOS = "ACTGATACACTCCAAGCGGAGACAGACCAACTAGAAGATGAGAAGTCTGCTTTGCAGACCGAGATTGCCAACCTGCTGAAGGAGAAGGAAAAACTA"),
+#'     nbrMutatedCodonsMaxForward = 1
+#' )
+#' ## Output sample
+#' outp <- digestFastqs(
+#'     fastqForward = system.file("extdata", "cisOutput_1.fastq.gz", 
+#'                                package = "mutscan"), 
+#'     elementsForward = "SUCV", elementLengthsForward = c(1, 10, 18, 96), 
+#'     constantForward = "AACCGGAGGAGGGAGCTG", 
+#'     wildTypeForward = c(FOS = "ACTGATACACTCCAAGCGGAGACAGACCAACTAGAAGATGAGAAGTCTGCTTTGCAGACCGAGATTGCCAACCTGCTGAAGGAGAAGGAAAAACTA"),
+#'     nbrMutatedCodonsMaxForward = 1
+#' )
+#' ## Combine
+#' se <- summarizeExperiment(
+#'     x = list(r1inp = inp, r1outp = outp), 
+#'     coldata = data.frame(Name = c("r1inp", "r1outp"), 
+#'                          Condition = c("input", "output"), 
+#'                          Replicate = c("rep1", "rep1")),
+#'     countType = "umis"
+#' )
+#' se
 #'
 summarizeExperiment <- function(x, coldata, countType = "umis") {
     ## --------------------------------------------------------------------------
