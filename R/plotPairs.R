d6c14533d15a64b894418b1c06d15c0ca4e4c8aa
Charlotte Soneson <charlottesoneson@gmail.com>
2025-06-02 20:19:59 +0200

    Cleanup, plotPairs.R

--- R/plotPairs.R
+++ R/plotPairs.R
@@ -41,11 +41,11 @@
 #' @param addIdentityLine Logical scalar, indicating whether the identity line 
 #'     should be added (only used if \code{pointsType = "points"}).
 #'
-#' @importFrom GGally eval_data_col ggpairs wrap
+#' @importFrom GGally eval_data_col ggpairs
 #' @importFrom ggplot2 ggplot annotate theme_void ylim stat_density2d
 #'     scale_fill_continuous geom_point theme_bw theme element_blank aes
 #'     geom_histogram scale_x_continuous scale_y_continuous geom_abline
-#'     after_stat
+#'     after_stat element_rect
 #' @importFrom stats cor
 #' @importFrom SummarizedExperiment assayNames assay
 #' @importFrom grDevices hcl.colors rgb colorRamp
@@ -62,10 +62,10 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
                       corSizeAdd = 2, pointSize = 0.1, pointAlpha = 0.3,
                       colorByCorrelation = TRUE, corrColorRange = NULL,
                       addIdentityLine = FALSE) {
-
+    
     .assertVector(x = se, type = "SummarizedExperiment")
     .assertScalar(x = selAssay, type = "character",
-                  validValues = SummarizedExperiment::assayNames(se))
+                  validValues = assayNames(se))
     .assertScalar(x = doLog, type = "logical")
     .assertScalar(x = pseudocount, type = "numeric", rngIncl = c(0, Inf))
     .assertScalar(x = corMethod, type = "character",
@@ -88,89 +88,97 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
     if (pointsType %in% c("scattermore", "scattermost")) {
         .assertPackagesAvailable("scattermore")
     }
-
-    ## ----------------------------------------------------------------------- ##
+    
+    ## --------------------------------------------------------------------- ##
     ## Define shared theme elements
-    ## ----------------------------------------------------------------------- ##
+    ## --------------------------------------------------------------------- ##
     ggtheme <- list(
         theme_bw(),
-        theme(panel.grid.major = ggplot2::element_blank(),
-              panel.grid.minor = ggplot2::element_blank())
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank())
     )
-
-    ## ----------------------------------------------------------------------- ##
+    
+    ## --------------------------------------------------------------------- ##
     ## Correlations
-    ## ----------------------------------------------------------------------- ##
-    ## Define function to calculate and display correlations (for use with ggpairs)
+    ## --------------------------------------------------------------------- ##
+    ## Define function to calculate and display correlations 
+    ## (for use with ggpairs)
     cor_fcn <- function(data, mapping, ...) {
         ## Get data
-        xData <- GGally::eval_data_col(data, mapping$x)
-        yData <- GGally::eval_data_col(data, mapping$y)
-
+        xData <- eval_data_col(data, mapping$x)
+        yData <- eval_data_col(data, mapping$y)
+        
         ## Calculate correlation
-        mainCor <- stats::cor(xData, yData, method = corMethod, 
-                              use = "pairwise.complete.obs")
-        transfCor <- (abs(mainCor) - min(corRange))/(max(corRange) - min(corRange))
+        mainCor <- cor(xData, yData, method = corMethod, 
+                       use = "pairwise.complete.obs")
+        transfCor <- (abs(mainCor) - min(corRange)) / 
+            (max(corRange) - min(corRange))
         ## Protect against numerical imprecision leading to values outside
         ## the allowed range
         transfCor <- min(1.0, transfCor)
         transfCor <- max(0.0, transfCor)
-
+        
         ## Determine the color
         if (colorByCorrelation) {
             if (mainCor >= 0) {
-                cols <- grDevices::hcl.colors(n = 11, palette = "RdBu")[6:2]
-                col <- grDevices::rgb(grDevices::colorRamp(
+                cols <- hcl.colors(n = 11, palette = "RdBu")[6:2]
+                col <- rgb(colorRamp(
                     cols)(transfCor),
                     maxColorValue = 255)
             } else {
-                cols <- grDevices::hcl.colors(n = 11, palette = "RdBu")[6:10]
-                col <- grDevices::rgb(grDevices::colorRamp(
+                cols <- hcl.colors(n = 11, palette = "RdBu")[6:10]
+                col <- rgb(colorRamp(
                     cols)(transfCor),
                     maxColorValue = 255)
             }
         } else {
             col <- "white"
         }
-
+        
         ## Construct plot
-        ggplot2::ggplot(data = data, mapping = mapping) +
-            ggplot2::annotate(x = 0.5, y = 0.5, label = round(mainCor, digits = 3),
-                              geom = "text",
-                              size = abs(mainCor) * corSizeMult + corSizeAdd) +
-            ggtheme + ggplot2::ylim(c(0, 1)) +
-            ggplot2::theme(panel.background = ggplot2::element_rect(fill = col))
+        ggplot(data = data, mapping = mapping) +
+            annotate(x = 0.5, y = 0.5, 
+                     label = round(mainCor, digits = 3),
+                     geom = "text",
+                     size = abs(mainCor) * corSizeMult + corSizeAdd) +
+            ggtheme + ylim(c(0, 1)) +
+            theme(
+                panel.background = element_rect(fill = col)
+            )
     }
-
+    
     ## ----------------------------------------------------------------------- ##
     ## Scatter plots
     ## ----------------------------------------------------------------------- ##
     if (pointsType == "smoothscatter") {
-        ## Define function to create smoothscatter-like plot (for use with ggpairs)
+        ## Define function to create smoothscatter-like plot 
+        ## (for use with ggpairs)
         smoothscat <- function(data, mapping, ...) {
-            ggplot2::ggplot(data = data, mapping = mapping) +
-                ggplot2::stat_density2d(
-                    ggplot2::aes(fill = ggplot2::after_stat(.data$density)^0.25), geom = "tile",
+            ggplot(data = data, mapping = mapping) +
+                stat_density2d(
+                    aes(fill = after_stat(
+                        .data$density)^0.25), geom = "tile",
                     contour = FALSE, n = 200) +
-                ggplot2::scale_fill_continuous(low = "white", high = "darkgreen") +
+                scale_fill_continuous(low = "white", 
+                                      high = "darkgreen") +
                 ggtheme +
-                ggplot2::scale_x_continuous(expand = c(0, 0)) +
-                ggplot2::scale_y_continuous(expand = c(0, 0))
+                scale_x_continuous(expand = c(0, 0)) +
+                scale_y_continuous(expand = c(0, 0))
         }
     } else if (pointsType == "points") {
         ## Define function to create scatter plot (for use with ggpairs)
         if (addIdentityLine) {
             plotpoints <- function(data, mapping, ...) {
-                ggplot2::ggplot(data = data, mapping = mapping) +
-                    ggplot2::geom_abline(slope = 1, intercept = 0, 
-                                         linetype = "dashed", color = "grey") + 
-                    ggplot2::geom_point(alpha = pointAlpha, size = pointSize) +
+                ggplot(data = data, mapping = mapping) +
+                    geom_abline(slope = 1, intercept = 0, 
+                                linetype = "dashed", color = "grey") + 
+                    geom_point(alpha = pointAlpha, size = pointSize) +
                     ggtheme
             }
         } else {
             plotpoints <- function(data, mapping, ...) {
-                ggplot2::ggplot(data = data, mapping = mapping) +
-                    ggplot2::geom_point(alpha = pointAlpha, size = pointSize) +
+                ggplot(data = data, mapping = mapping) +
+                    geom_point(alpha = pointAlpha, size = pointSize) +
                     ggtheme
             }
         }
@@ -178,16 +186,16 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
         ## Define function to create scattermore plot (for use with ggpairs)
         if (addIdentityLine) {
             plotscattermore <- function(data, mapping, ...) {
-                ggplot2::ggplot(data = data, mapping = mapping) +
-                    ggplot2::geom_abline(slope = 1, intercept = 0, 
-                                         linetype = "dashed", color = "grey") + 
+                ggplot(data = data, mapping = mapping) +
+                    geom_abline(slope = 1, intercept = 0, 
+                                linetype = "dashed", color = "grey") + 
                     scattermore::geom_scattermore(alpha = pointAlpha, 
                                                   pointsize = pointSize) +
                     ggtheme
             }
         } else {
             plotscattermore <- function(data, mapping, ...) {
-                ggplot2::ggplot(data = data, mapping = mapping) +
+                ggplot(data = data, mapping = mapping) +
                     scattermore::geom_scattermore(alpha = pointAlpha, 
                                                   pointsize = pointSize) +
                     ggtheme
@@ -198,12 +206,12 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
         if (addIdentityLine) {
             plotscattermost <- function(data, mapping, ...) {
                 ## Get data
-                xData <- GGally::eval_data_col(data, mapping$x)
-                yData <- GGally::eval_data_col(data, mapping$y)
+                xData <- eval_data_col(data, mapping$x)
+                yData <- eval_data_col(data, mapping$y)
                 
-                ggplot2::ggplot(data = data, mapping = mapping) +
-                    ggplot2::geom_abline(slope = 1, intercept = 0, 
-                                         linetype = "dashed", color = "grey") + 
+                ggplot(data = data, mapping = mapping) +
+                    geom_abline(slope = 1, intercept = 0, 
+                                linetype = "dashed", color = "grey") + 
                     scattermore::geom_scattermost(xy = cbind(xData, yData), 
                                                   pointsize = pointSize) +
                     ggtheme
@@ -211,10 +219,10 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
         } else {
             plotscattermost <- function(data, mapping, ...) {
                 ## Get data
-                xData <- GGally::eval_data_col(data, mapping$x)
-                yData <- GGally::eval_data_col(data, mapping$y)
+                xData <- eval_data_col(data, mapping$x)
+                yData <- eval_data_col(data, mapping$y)
                 
-                ggplot2::ggplot(data = data, mapping = mapping) +
+                ggplot(data = data, mapping = mapping) +
                     scattermore::geom_scattermost(xy = cbind(xData, yData), 
                                                   pointsize = pointSize) +
                     ggtheme
@@ -232,28 +240,32 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
     } else if (pointsType == "scattermost") {
         lower <- list(continuous = plotscattermost)
     }
-
-    ## ----------------------------------------------------------------------- ##
+    
+    ## --------------------------------------------------------------------- ##
     ## Histogram
-    ## ----------------------------------------------------------------------- ##
+    ## --------------------------------------------------------------------- ##
     diaghist <- function(data, mapping, ...) {
-        gg <- ggplot2::ggplot(data = data, mapping = mapping) +
-            ggplot2::geom_histogram(fill = "#F5C710", color = "grey50", bins = histBreaks) +
+        gg <- ggplot(data = data, mapping = mapping) +
+            geom_histogram(fill = "#F5C710", color = "grey50", 
+                           bins = histBreaks) +
             ggtheme
         if (pointsType == "smoothscatter") {
-            gg <- gg + ggplot2::scale_x_continuous(expand = c(0, 0))
+            gg <- gg + scale_x_continuous(expand = c(0, 0))
         }
         gg
     }
-
-    ## ----------------------------------------------------------------------- ##
+    
+    ## --------------------------------------------------------------------- ##
     ## Combined plot
-    ## ----------------------------------------------------------------------- ##
-    ## Prepare the data and plot title, depending on whether to log-transform or not
+    ## --------------------------------------------------------------------- ##
+    ## Prepare the data and plot title, depending on whether to log-transform 
+    ## or not
     if (doLog) {
-        mat <- log10(as.matrix(SummarizedExperiment::assay(se, selAssay)) + pseudocount)
+        mat <- log10(as.matrix(assay(se, selAssay)) + 
+                         pseudocount)
         title <- paste0("log10(", selAssay,
-                        ifelse(pseudocount > 0, paste0(" + ", pseudocount), ""),
+                        ifelse(pseudocount > 0, 
+                               paste0(" + ", pseudocount), ""),
                         ")")
     } else {
         mat <- as.matrix(assay(se, selAssay))
@@ -262,21 +274,23 @@ plotPairs <- function(se, selAssay = "counts", doLog = TRUE, pseudocount = 1,
     mat <- as.data.frame(mat)
     title <- paste0(title, ", ", toupper(substr(corMethod, 1, 1)),
                     substr(corMethod, 2, nchar(corMethod)), " correlation")
-
+    
     ## Calculate correlations and get range
-    allCors <- stats::cor(mat, method = corMethod, use = "pairwise.complete.obs")
+    allCors <- cor(mat, method = corMethod, 
+                   use = "pairwise.complete.obs")
     if (!is.null(corrColorRange)) {
         corRange <- corrColorRange
     } else {
         corRange <- range(abs(allCors[upper.tri(allCors)]))
     }
     if (length(unique(corRange)) == 1) {
-        ## Having a range with width 0 leads to problems in the rescaling of the correlations
+        ## Having a range with width 0 leads to problems in the rescaling of 
+        ## the correlations
         corRange <- c(0, 1)
     }
-
+    
     ## Construct the pairs plot
-    GGally::ggpairs(
+    ggpairs(
         data = mat,
         mapping = NULL,
         columns = seq_len(ncol(mat)),
