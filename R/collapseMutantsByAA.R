182bebca18950dd66bfa756d4acb5131a9111fb2
Charlotte Soneson <charlottesoneson@gmail.com>
2025-06-02 20:34:51 +0200

    Import rowData<-

--- R/collapseMutantsByAA.R
+++ R/collapseMutantsByAA.R
@@ -157,6 +157,7 @@ collapseMutantsByAA <- function(se) {
 #' @importFrom DelayedArray rowsum
 #' @importFrom S4Vectors metadata DataFrame
 #' @importFrom SummarizedExperiment assays rowData SummarizedExperiment colData
+#'     rowData<-
 #' @importFrom dplyr across group_by summarize full_join
 #' @importFrom stats setNames
 #'
